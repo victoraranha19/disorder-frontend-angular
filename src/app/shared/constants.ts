9b1675f2a7055d7098e7f927100510db2a3666f1
Victor Aranha <aldo.victor1610@gmail.com>
2026-01-20 23:55:40 -0300

    fix api url
    Signed-off-by: Victor Aranha <aldo.victor1610@gmail.com>

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,2 +1,2 @@
-// export const API_URL_BASE = 'https://disorder-backend-springboot.onrender.com/api';
-export const API_URL_BASE = 'http://localhost:8080/api';
+export const API_URL_BASE = 'https://disorder-backend-springboot.onrender.com/api';
+// export const API_URL_BASE = 'http://localhost:8080/api';
