d41fb19a50bfa4d71f55c7671afc2f1725102885
Victor Aranha <aldo.victor1610@gmail.com>
2026-01-27 06:17:54 -0300

    test: add mock user data and improve AssessoriaComponent tests
    Signed-off-by: Victor Aranha <aldo.victor1610@gmail.com>

--- src/app/pages/assessoria/assessoria.component.spec.ts
+++ src/app/pages/assessoria/assessoria.component.spec.ts
@@ -1,16 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AssessoriaComponent } from './assessoria.component';
+import { UsuariosService } from '../../services/usuarios.service';
+import { of } from 'rxjs';
+import { usuarioMock } from '../../shared/mockTests';
 
 describe('AssessoriaComponent', () => {
   let component: AssessoriaComponent;
   let fixture: ComponentFixture<AssessoriaComponent>;
 
+  let usuarioSSPy: jasmine.SpyObj<UsuariosService>;
+
   beforeEach(async () => {
+    usuarioSSPy = jasmine.createSpyObj<UsuariosService>('UsuariosService', ['meuPerfil$']);
+    usuarioSSPy.meuPerfil$.and.returnValue(of(usuarioMock));
+
     await TestBed.configureTestingModule({
-      imports: [AssessoriaComponent]
-    })
-    .compileComponents();
+      imports: [AssessoriaComponent],
+      providers: [{ provide: UsuariosService, useValue: usuarioSSPy }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AssessoriaComponent);
     component = fixture.componentInstance;
---
+++ src/app/shared/mockTests.ts
@@ -0,0 +1,8 @@
+import { IUsuario } from './interfaces';
+
+export const usuarioMock: IUsuario = {
+  email: 'emailtest',
+  nomeCompleto: 'Nome Teste',
+  telefone: '11999999999',
+  chavePix: 'chavepixteste',
+};
